29b0987099e0fd6efe80fbc3c69a43a1da29ccd1
Mark Artishuk <markyshuk@gmail.com>
2019-11-11 11:35:19 -0800

    Added display tests and snapshot for Display component

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,21 @@
 // Test away!
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import Display from './Display';
+
+
+test("it renders correctly ", ()=>{
+    expect(render(<Display />)).toMatchSnapshot();
+});
+
+describe("Displays correct data depending on props", ()=> {
+    it("displays closed if closed", ()=> {
+        const {getByText} = render(<Display closed/>);
+        getByText(/Closed/i)
+    });
+
+    it("displays open if open", ()=> {
+        const {getByText} = render(<Display />);
+        getByText(/Open/i)
+    })
+});
