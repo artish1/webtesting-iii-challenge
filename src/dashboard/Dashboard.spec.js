4f71b9a5fe3e7a41d0c61fcee2bceb3c4c50784f
Mark Artishuk <markyshuk@gmail.com>
2019-11-11 12:08:58 -0800

    Added rendered display for Dashboard

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,12 @@
+import React from 'react';
+import {render, getByTestId} from "@testing-library/react";
+
+import Dashboard from "../dashboard/Dashboard";
 // Test away
+
+test("renders dashboard according to snapshot", ()=> {
+    expect(render(<Dashboard />)).toMatchSnapshot();
+});
+
+
+
