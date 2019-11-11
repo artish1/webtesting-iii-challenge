9c67b35226611a06cb0405bb725bd85943e1077a
Mark Artishuk <markyshuk@gmail.com>
2019-11-11 13:59:36 -0800

    Added and finished tests for control buttons, also added default state test

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -8,5 +8,17 @@ test("renders dashboard according to snapshot", ()=> {
     expect(render(<Dashboard />)).toMatchSnapshot();
 });
 
+describe("Defaults to unlocked/open state", () => {
 
+    it("defaults to unlocked gate", ()=> {
+        const {getByText} = render(<Dashboard />);
+        getByText(/Unlocked/);
+    });
+
+    it("defaults to open gate", ()=> {
+        const {getByText} = render(<Dashboard />);
+        getByText(/Open/);
+    })
+    
+});
 
